e72cdbb6ed5bc820eabd412ef4c417c90d1007a9
zarrar <zarrarmalik81@gmail.com>
2019-01-23 18:45:00 +0000

    Using @onetomany and @joinColumn

---
+++ project-security-jpa/src/main/java/com/myself/ProjectSecurityJpaApplication.java
@@ -0,0 +1,14 @@
+package com.myself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSecurityJpaApplication.class, args);
+	}
+
+}
+
---
+++ project-security-jpa/src/test/java/com/myself/ProjectSecurityJpaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.myself;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectSecurityJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
