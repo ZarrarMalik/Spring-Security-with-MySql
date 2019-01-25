1af121a8f71046392c190ed964989d8606be853f
zarrar <zarrarmalik81@gmail.com>
2019-01-25 18:37:36 +0000

    User Details Service

--- project-security-jpa/src/main/java/com/myself/repository/UserRepository.java
+++ project-security-jpa/src/main/java/com/myself/repository/UserRepository.java
@@ -6,4 +6,7 @@ import com.myself.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 
+
+	User findByUsername(String username);
+
 }
---
+++ project-security-jpa/src/main/java/com/myself/service/CustomerUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.myself.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.myself.model.User;
+import com.myself.repository.UserRepository;
+
+public class CustomerUserDetailsService implements UserDetailsService{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+		CustomerUserDetails userDetails;
+		if (user != null) {
+			userDetails = new CustomerUserDetails();
+			userDetails.setUser(user);
+	}else {
+		throw new UsernameNotFoundException("User not exist with name : " + username);
+	}
+		return userDetails;
+}
+}
